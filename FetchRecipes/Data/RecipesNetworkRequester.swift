47938b0d8d4204c34753f26f4b6324bb36de1468
Coleton Gorecke <coletoncodes@gmail.com>
2024-11-06 14:02:20 -0600

    Added RecipesNetworkRequester Implementation

--- FetchRecipes/Data/RecipesNetworkRequester.swift
+++ FetchRecipes/Data/RecipesNetworkRequester.swift
@@ -10,33 +10,23 @@ import Foundation
 import Networking
 
 protocol RecipesNetworkRequesting {
-    func getRecipes() async throws -> NetworkResponse<[RecipeDTO], EmptyErrorResponse>
+    func getRecipes() async throws -> NetworkResponse<GetRecipesRequest.SuccessResponse, EmptyErrorResponse>
 }
 
 final class RecipesNetworkRequester: RecipesNetworkRequesting {
     private let getRecipesResponseHandler: RecipesResponseHandler<GetRecipesRequest.SuccessResponse>
+    @Injected(\DataContainer.networkRequester) private var networkRequester
 
     init() {
         self.getRecipesResponseHandler = DataContainer.shared.makeRecipesResponseHandler(successResponse: GetRecipesRequest.SuccessResponse.self).resolve()
     }
 
-//    func getActivities() async throws -> NetworkResponse<[ActivityDTO], SimpleErrorResponse> {
-//        do {
-//            let responseHandler = ActivitiesResponseHandler<GetActivitiesRequest.SuccessResponse>(decoder: decoder)
-//            return try await requester.performRequest(GetActivitiesRequest(), responseHandler: responseHandler)
-//        } catch {
-//            log("Failed to handle request with error: \(error)", .error, .networking)
-//            throw error
-//        }
-//    }
-
-    func getRecipes() async throws -> NetworkResponse<[RecipeDTO], EmptyErrorResponse> {
-        fatalError("unimplemented")
-//        do {
-////            let recipes = try await getRecipesResponseHandler.handle(request: GetRecipesRequest())
-//
-//        } catch {
-//
-//        }
+    func getRecipes() async throws -> NetworkResponse<GetRecipesRequest.SuccessResponse, EmptyErrorResponse> {
+        do {
+            return try await networkRequester.performRequest(GetRecipesRequest(), responseHandler: getRecipesResponseHandler)
+        } catch {
+            log("Failed to get recipes with error: \(error)", .error, .networking)
+            throw error
+        }
     }
 }
