0b32f94b8beb08422d860f9df53e3bc49bde26b7
Coleton Gorecke <coletoncodes@gmail.com>
2024-11-06 10:05:33 -0600

    Initial Project Commit

---
+++ FetchRecipes/FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Coleton Gorecke on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
